788b6819b31d06d0d4fb0e9d6c949dc65accbdd5
Santiago Mancera <dsmr19961@gmail.com>
2023-06-28 22:08:03 -0500

    Agregando todos los end-points

--- src/config/ConfigApi.js
+++ src/config/ConfigApi.js
@@ -1,6 +1,21 @@
 const ConfigApi = {
     getToken:{
         url:'https://apiestudiantes.maosystems.dev/tokens',
+    },
+    getAllStudents: {
+        url:'https://apiestudiantes.maosystems.dev/estudiantes',
+    },
+    createStudent:{
+        url:'https://apiestudiantes.maosystems.dev/estudiantes',
+    },
+    getStudent:{
+        url:'https://apiestudiantes.maosystems.dev/estudiantes/',
+    },
+    updateStudent:{
+        url:'https://apiestudiantes.maosystems.dev/estudiantes/',
+    },
+    updateStateStudent:{
+        url:'https://apiestudiantes.maosystems.dev/estudiantes/estado/',
     }
 }
 
